2772769b9614b15410b58280071c978627af9296
Can Karatepe <cankaratepe2000@gmail.com>
2020-12-03 00:14:37 +0300

    Fixed wrong argument type in readDatasets()

--- DataLabelingSystem/src/main/java/com/DataLabelingSystem/DataLabelingSystem.java
+++ DataLabelingSystem/src/main/java/com/DataLabelingSystem/DataLabelingSystem.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class DataLabelingSystem {
 
 	public static void main(String[] args) {
-	JsonParser jsonParser= JsonParser.getJsonParser();
-		ArrayList<Dataset> datasets = jsonParser.readDatasets("input-1.json"); //TODO json files will be called
+		JsonParser jsonParser = JsonParser.getJsonParser();
+		ArrayList<Dataset> datasets = jsonParser.readDatasets(new String[]{"input-1.json", "input-2.json"});
 	}
 
 }
