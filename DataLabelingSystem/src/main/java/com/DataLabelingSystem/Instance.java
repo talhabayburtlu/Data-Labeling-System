bfd21cb3ef0c5d21af108c4c67c2a0f794c60d90
osmanerikci <erikciosmann@gmail.com>
2020-12-02 20:51:20 +0300

    Instance, Label created.

---
+++ DataLabelingSystem/src/main/java/com/DataLabelingSystem/Instance.java
@@ -0,0 +1,42 @@
+package DataLabelingSystem.src.main.java.com.DataLabelingSystem;
+
+public class Instance {
+
+    private int id;
+    private String content;
+    private Dataset dataset;
+
+    Instance(int id, String content){
+        this.id = id;
+        this.content = content;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Dataset getDataset() {
+        return dataset;
+    }
+
+    public void setDataset(Dataset dataset) {
+        this.dataset = dataset;
+    }
+
+    private Dataset dataset;
+
+
+}
---
+++ DataLabelingSystem/src/main/java/com/DataLabelingSystem/Label.java
@@ -0,0 +1,37 @@
+package DataLabelingSystem.src.main.java.com.DataLabelingSystem;
+
+public class Label {
+
+    private int id;
+    private String text;
+    private Dataset dataset;
+
+    Label(int id, String text){
+        this.id = id;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Dataset getDataset() {
+        return dataset;
+    }
+
+    public void setDataset(Dataset dataset) {
+        this.dataset = dataset;
+    }
+}
