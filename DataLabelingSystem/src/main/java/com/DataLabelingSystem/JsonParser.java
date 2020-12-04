160267dc44dd853d0987e54994e6ecd1a11b671c
Can Karatepe <cankaratepe2000@gmail.com>
2020-12-05 00:33:53 +0300

    Implemented readDatasets method

--- DataLabelingSystem/src/main/java/com/DataLabelingSystem/JsonParser.java
+++ DataLabelingSystem/src/main/java/com/DataLabelingSystem/JsonParser.java
@@ -1,6 +1,12 @@
 package com.DataLabelingSystem;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class JsonParser {
     private static final JsonParser instance = new JsonParser();
@@ -12,8 +18,21 @@ public class JsonParser {
         return instance;
     }
 
-    public ArrayList<Dataset> readDatasets(String[] filenames) {
-        throw new UnsupportedOperationException("This method has not been implemented yet.");
+    public ArrayList<Dataset> readDatasets(String[] filenames) throws FileNotFoundException, JsonProcessingException {
+        ArrayList<Dataset> datasets = new ArrayList<>();
+        for (String filename :
+                filenames) {
+            Scanner fileScanner = new Scanner(new File(filename));
+            StringBuilder sb = new StringBuilder();
+            while (fileScanner.hasNextLine()) {
+                sb.append(fileScanner.nextLine());
+            }
+            String jsonString = sb.toString();
+            ObjectMapper objectMapper = new ObjectMapper();
+            Dataset dataset = objectMapper.readValue(jsonString, Dataset.class);
+            datasets.add(dataset);
+        }
+        return datasets;
     }
 
     public ArrayList<User> readUsers(String[] filenames) {
