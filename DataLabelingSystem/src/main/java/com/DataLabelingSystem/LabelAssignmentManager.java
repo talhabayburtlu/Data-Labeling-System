be4e285b6a9b061c01304d5fa7812b174fe69f67
neur1 <49001811+neur1@users.noreply.github.com>
2020-12-04 22:00:55 +0300

    With Singleton Design Pattern, LabelAssignmentManager created to control label assignment creation.

---
+++ DataLabelingSystem/src/main/java/com/DataLabelingSystem/LabelAssignmentManager.java
@@ -0,0 +1,34 @@
+package com.DataLabelingSystem;
+
+public class LabelAssignmentManager {
+    private static LabelAssignmentManager labelAssignmentManager;
+
+    private LabelAssignmentManager() { }
+
+    private static LabelAssignmentManager createInstance() {
+         return new LabelAssignmentManager();
+    }
+
+    public static LabelAssignmentManager getLabelAssignmentManager() {
+        if (labelAssignmentManager == null)
+            labelAssignmentManager = createInstance();
+        return labelAssignmentManager;
+    }
+
+    public LabelAssignment createLabelAssignment(User user, Instance instance, Label[] labels) {
+        return new LabelAssignment(user,instance,labels);
+    }
+
+    public void addToDataset(LabelAssignment labelAssignment) {
+        Instance instance = labelAssignment.getInstance();
+        Dataset dataset = instance.getDataset(); //get dataset from instance
+        dataset.getLabelAssignments().add(labelAssignment);
+    }
+
+    public void removeFromDataset(LabelAssignment labelAssignment) {
+        Instance instance = labelAssignment.getInstance();
+        Dataset dataset = instance.getDataset(); //get dataset from instance
+        dataset.getLabelAssignments().remove(labelAssignment);
+    }
+
+}
