ff9110b2bb486d8d4bec4865074374e70b1de905
aEmreAydemir <45498427+aEmreAydemir@users.noreply.github.com>
2020-12-05 03:44:49 +0300

    Implemented main method

--- DataLabelingSystem/src/main/java/com/DataLabelingSystem/DataLabelingSystem.java
+++ DataLabelingSystem/src/main/java/com/DataLabelingSystem/DataLabelingSystem.java
@@ -1,12 +1,27 @@
 package com.DataLabelingSystem;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class DataLabelingSystem {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, JsonProcessingException {
 		JsonParser jsonParser = JsonParser.getJsonParser();
 		ArrayList<Dataset> datasets = jsonParser.readDatasets(new String[]{"input-1.json", "input-2.json"});
+		ArrayList<User> users = jsonParser.readUsers("users.json");
+		for (Dataset dataset :
+				datasets) {
+			for (User user :
+					users) {
+				for (Instance instance :
+						dataset.getInstances()) {
+					user.labelWithMechanism(instance, dataset.getLabels().toArray(new Label[0]));
+				}
+			}
+		}
+		System.out.println("Simulation complete.");
 	}
 
 }
