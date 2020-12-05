9e25cac35d9d1d1a9585b4b28d07f6c147a9b79c
talhabayburtlu <talha_bayburtlu2@hotmail.com>
2020-12-05 14:28:21 +0300

    Removed setter methods from LabelAssignment for making it immutable.

--- DataLabelingSystem/src/main/java/com/DataLabelingSystem/LabelAssignment.java
+++ DataLabelingSystem/src/main/java/com/DataLabelingSystem/LabelAssignment.java
@@ -27,32 +27,16 @@ public class LabelAssignment {
         return instance;
     }
 
-    public void setInstance(Instance instance) {
-        this.instance = instance;
-    }
-
     public Label[] getLabels() {
         return labels;
-    }
-
-    public void setLabels(Label[] labels) {
-        this.labels = labels;
     }
 
     public User getUser() {
         return user;
     }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
-
     public Date getDatetime() {
         return datetime;
-    }
-
-    public void setDatetime(Date datetime) {
-        this.datetime = datetime;
     }
 
     @JsonGetter("instance id")
