9a293bdecb65f7baa641d403c52cc7317ea25fec
Can Karatepe <cankaratepe2000@gmail.com>
2020-12-05 03:52:18 +0300

    Implement readUsers method.

--- DataLabelingSystem/src/main/java/com/DataLabelingSystem/JsonParser.java
+++ DataLabelingSystem/src/main/java/com/DataLabelingSystem/JsonParser.java
@@ -2,10 +2,12 @@ package com.DataLabelingSystem;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class JsonParser {
@@ -22,12 +24,7 @@ public class JsonParser {
         ArrayList<Dataset> datasets = new ArrayList<>();
         for (String filename :
                 filenames) {
-            Scanner fileScanner = new Scanner(new File(filename));
-            StringBuilder sb = new StringBuilder();
-            while (fileScanner.hasNextLine()) {
-                sb.append(fileScanner.nextLine());
-            }
-            String jsonString = sb.toString();
+            String jsonString = readAllLines(filename);
             ObjectMapper objectMapper = new ObjectMapper();
             Dataset dataset = objectMapper.readValue(jsonString, Dataset.class);
             datasets.add(dataset);
@@ -35,8 +32,22 @@ public class JsonParser {
         return datasets;
     }
 
-    public ArrayList<User> readUsers(String[] filenames) {
-        throw new UnsupportedOperationException("This method has not been implemented yet.");
+    public ArrayList<User> readUsers(String filename) throws FileNotFoundException, JsonProcessingException {
+        String jsonString = readAllLines(filename);
+        ObjectMapper objectMapper = new ObjectMapper();
+        User[] parsedUsers = objectMapper.readValue(jsonString, User[].class);
+        return new ArrayList<>(Arrays.asList(parsedUsers));
+    }
+
+    @NotNull
+    private String readAllLines(String filename) throws FileNotFoundException {
+        Scanner fileScanner = new Scanner(new File(filename));
+        StringBuilder sb = new StringBuilder();
+        while (fileScanner.hasNextLine()) {
+            sb.append(fileScanner.nextLine());
+        }
+        fileScanner.close();
+        return sb.toString();
     }
 
     public void writeDatasetsWithUsers(String[] filenames, ArrayList<Dataset> datasets, ArrayList<User> users) {
