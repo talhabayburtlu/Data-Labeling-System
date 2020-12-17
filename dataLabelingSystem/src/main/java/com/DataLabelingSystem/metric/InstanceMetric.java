baa183938ed4612fb352b14650f851c2fbb57f5d
talhabayburtlu <talha_bayburtlu2@hotmail.com>
2020-12-17 21:44:44 +0300

    Created instance metric class.

---
+++ dataLabelingSystem/src/main/java/com/DataLabelingSystem/metric/InstanceMetric.java
@@ -0,0 +1,42 @@
+package com.DataLabelingSystem.metric;
+
+import com.DataLabelingSystem.assignment.LabelAssignment;
+import com.DataLabelingSystem.model.Instance;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class InstanceMetric {
+
+    private Instance instance;
+    private ArrayList<LabelAssignment> labelAssignments;
+
+    public InstanceMetric(Instance instance) {
+        this.instance = instance;
+    }
+
+    public void updateLabelAssignments() {
+        ArrayList<LabelAssignment> labelAssignments = this.instance.getDataset().getLabelAssignments()
+                .stream()
+                .filter(l -> l.getInstance() == this.instance)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        this.labelAssignments = labelAssignments;
+    }
+
+    public Instance getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Instance instance) {
+        this.instance = instance;
+    }
+
+    public ArrayList<LabelAssignment> getLabelAssignments() {
+        return labelAssignments;
+    }
+
+    public void setLabelAssignments(ArrayList<LabelAssignment> labelAssignments) {
+        this.labelAssignments = labelAssignments;
+    }
+}
