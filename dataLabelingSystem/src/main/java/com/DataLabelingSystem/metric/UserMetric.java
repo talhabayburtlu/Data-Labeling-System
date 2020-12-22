9ad4e174f03bb5cec1ce1ada84f6f4bdc1702179
zekeriyaBatuhanKaratas <zbatuhankaratas@gmail.com>
2020-12-22 16:34:05 +0300

    The methods of UserMetric Class have been updated.

--- dataLabelingSystem/src/main/java/com/DataLabelingSystem/metric/UserMetric.java
+++ dataLabelingSystem/src/main/java/com/DataLabelingSystem/metric/UserMetric.java
@@ -3,6 +3,7 @@ package com.DataLabelingSystem.metric;
 import com.DataLabelingSystem.assignment.LabelAssignment;
 import com.DataLabelingSystem.model.Dataset;
 import com.DataLabelingSystem.model.Instance;
+import com.DataLabelingSystem.model.Label;
 import com.DataLabelingSystem.model.User;
 
 import java.util.ArrayList;
@@ -21,18 +22,66 @@ public class UserMetric {
         return getNumberOfDatasets;
     }
 
-    public HashMap<Dataset,Integer> getDatasetWithCompletenessPercentage() {
-        return null;
+    public HashMap<Dataset,Integer> getDatasetWithCompletenessPercentage(){
+        int completenessPercentage;
+        for (int i = 0; i <  user.getAssignedDatasets().size() ; i++) {
+            Dataset dataset = user.getAssignedDatasets().get(i);
+            dataset.getDatasetMetric().getCompletenessPercentage();
+        }
     }
     public int getInstancesLabeledCount(){
-        return 0;
+        int InstanceCounter = 0;
+
+        for (int i = 0; i <  user.getAssignedDatasets().size() ; i++) {
+            Dataset dataset = user.getAssignedDatasets().get(i);
+
+            for (int j = 0; j < dataset.getLabelAssignments().size(); j++) {
+                LabelAssignment labelAssignment = dataset.getLabelAssignments().get(j);
+                if(labelAssignment.getUser().getId()==getUser().getId()){
+                    InstanceCounter++;
+                }
+            }
+        }
+        return InstanceCounter;
     }
+
     public int getUniqueInstancesLabeledCount(){
-        return 0;
+        int InstanceCounter = 0;
+        ArrayList<Integer> tempInstances = new ArrayList<>();
+
+        for (int i = 0; i <  user.getAssignedDatasets().size() ; i++) {
+            Dataset dataset = user.getAssignedDatasets().get(i);
+
+            for (int j = 0; j < dataset.getLabelAssignments().size(); j++) {
+                LabelAssignment labelAssignment = dataset.getLabelAssignments().get(j);
+                if(labelAssignment.getUser().getId()==getUser().getId()){
+                    if(!tempInstances.contains(labelAssignment.getInstanceId())){
+                        tempInstances.add(labelAssignment.getInstanceId());
+                    }
+                }
+            }
+        }
+        InstanceCounter = tempInstances.size();
+        return InstanceCounter;
     }
+
     public double getConsistencyPercentage(){
+        int datasetNumber = user.getAssignedDatasets().size();
+        ArrayList<Label> tempLabels = new ArrayList<Label>();
+
+
+        for (int i = 0; i <  user.getAssignedDatasets().size() ; i++) {
+            Dataset dataset = user.getAssignedDatasets().get(i);
+            for (int j = 0; j < dataset.getLabelAssignments().size(); j++) {
+                LabelAssignment labelAssignment = dataset.getLabelAssignments().get(j);
+                if(labelAssignment.getUser().getId()==getUser().getId()){
+                    j=0;
+                }
+                }
+
+            }
         return 0;
-    }
+        }
     public double getAverageLabelTime(){
         return 0;
     }
@@ -40,4 +89,22 @@ public class UserMetric {
         return 0;
     }
 
-}
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ArrayList<LabelAssignment> getLabelAssignments() {
+        return labelAssignments;
+    }
+
+    public void setLabelAssignments(ArrayList<LabelAssignment> labelAssignments) {
+        this.labelAssignments = labelAssignments;
+    }
+
+    }
+
+
